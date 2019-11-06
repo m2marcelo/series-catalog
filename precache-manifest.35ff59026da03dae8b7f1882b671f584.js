f2a73207f08517a7ef769d93efaf66ab4a8881fb
Marcelo Morais <m2marcelo@gmail.com>
2019-11-06 21:00:20 +0100

    Updates

---
+++ precache-manifest.35ff59026da03dae8b7f1882b671f584.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "558c3297b660240b2439004f4df48896",
+    "url": "/series-catalog/index.html"
+  },
+  {
+    "revision": "fa9e46e126f3e825c2b7",
+    "url": "/series-catalog/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ce6ddb7499bb31e1710a",
+    "url": "/series-catalog/static/js/2.af14c282.chunk.js"
+  },
+  {
+    "revision": "fa9e46e126f3e825c2b7",
+    "url": "/series-catalog/static/js/main.232b9315.chunk.js"
+  },
+  {
+    "revision": "2231e6105751d7fbe1e0",
+    "url": "/series-catalog/static/js/runtime-main.87c935b8.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/series-catalog/static/media/logo.25bf045c.svg"
+  }
+]);
