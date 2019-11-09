81e727af4ba8b89ca9731a0438d07d178b60ec35
Marcelo Morais <m2marcelo@gmail.com>
2019-11-09 10:22:18 +0100

    Updates

---
+++ precache-manifest.6f998ad5aa33b286abb02c7f8759829c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66adf7eac6a7949d48a292b407181758",
+    "url": "/serier/samtliga/index.html"
+  },
+  {
+    "revision": "0ede687a95f1097b7f6c",
+    "url": "/serier/samtliga/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c76cc213ab6a104d861b",
+    "url": "/serier/samtliga/static/js/2.c2c12e7a.chunk.js"
+  },
+  {
+    "revision": "0ede687a95f1097b7f6c",
+    "url": "/serier/samtliga/static/js/main.f3dd0d9a.chunk.js"
+  },
+  {
+    "revision": "2eaccf9964bf3c5bc271",
+    "url": "/serier/samtliga/static/js/runtime-main.e71d2d30.js"
+  }
+]);
