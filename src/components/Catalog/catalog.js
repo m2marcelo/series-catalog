7a9413eef0cf3cd17d9b5e5aa4a15f896e768301
Marcelo Morais <m2marcelo@gmail.com>
2019-11-10 18:35:38 +0100

    Adding the first draft for series catalog component.

---
+++ src/components/Catalog/catalog.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import Carousel from 'react-multi-carousel';
+import PropTypes from 'prop-types'
+import SeriesItem from '../SeriesItem/seriesItem'
+
+import * as SeriesParser from '../../utils/SeriesParser'
+
+import 'react-multi-carousel/lib/styles.css';
+import './style.css'
+
+
+class Catalog extends Component {
+  static propTypes = {
+    series: PropTypes.array,
+  }
+
+  render() {
+    const { series } = this.props
+
+    return (
+        <Carousel
+        additionalTransfrom={0}
+        arrows
+        autoPlaySpeed={3000}
+        centerMode={false}
+        className=""
+        containerClass="container-with-dots"
+        dotListClass=""
+        draggable
+        focusOnSelect={false}
+        infinite
+        itemClass=""
+        keyBoardControl
+        minimumTouchDrag={80}
+        renderButtonGroupOutside={true}
+        renderDotsOutside={true}
+        responsive={{
+          desktop: {
+            breakpoint: {
+              max: 3000,
+              min: 1024
+            },
+            items: 5,
+            partialVisibilityGutter: 40
+          },
+        }}
+        showDots={false}
+        sliderClass=""
+        slidesToSlide={1}
+        swipeable
+      >
+            {series.length !== 0 && series.map((serie) => (
+                <div  key={SeriesParser.getPublicPath(serie)} >
+                    <SeriesItem series={serie} />
+                </div>
+            ))}
+      </Carousel>
+    )
+  }
+}
+
+export default Catalog
